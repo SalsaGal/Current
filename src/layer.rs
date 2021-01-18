7e50a14d1687f7fdbe11b7ef0dc8ed23f6f7a3cf
SalsaGal <fielddashos@gmail.com>
2021-01-18 14:58:27 +1100

    Basic layer code

---
+++ src/layer.rs
@@ -0,0 +1,13 @@
+pub trait GameLayer {
+	// When the layer is added to the stack
+	fn on_push(&mut self) {}
+	// When the layer is taken off the stack
+	fn on_pop(&mut self) {}
+	
+	// When the layer is at the top of the stack
+	fn update_focused(&mut self) {}
+	// When the layer is not at the top of the stack
+	fn update_unfocused(&mut self) {}
+	// When the layer is anywhere
+	fn update(&mut self) {}
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,13 @@
 pub mod math;
+pub mod layer;
 
 use math::Vector2;
+use layer::GameLayer;
 
 use sdl2::{
+	event::{
+		Event,
+	},
 	EventPump,
 	render::{
 		WindowCanvas,
@@ -15,10 +20,14 @@ use sdl2::{
 pub struct Engine {
 	canvas: WindowCanvas,
 	event_pump: EventPump,
+
+	layer_stack: Vec<Box<dyn GameLayer>>,
+
+	pub running: bool,
 }
 
 impl Engine {
-	pub fn new(window_title: &str, window_bounds: Vector2<u32>) -> Self {
+	pub fn new(window_title: &str, window_bounds: Vector2<u32>, mut layer: Box<dyn GameLayer>) -> Self {
 		let sdl2_context = sdl2::init().unwrap();
 		let sdl2_video = sdl2_context.video().unwrap();
 		let sdl2_window = WindowBuilder::new(&sdl2_video, window_title, window_bounds.x, window_bounds.y).build().unwrap();
@@ -26,9 +35,34 @@ impl Engine {
 		Self {
 			canvas: sdl2_window.into_canvas().accelerated().present_vsync().build().unwrap(),
 			event_pump: sdl2_context.event_pump().unwrap(),
+
+			layer_stack: {
+				layer.on_push();
+				vec![ layer ]
+			},
+
+			running: true
 		}
 	}
 
 	pub fn update(&mut self) {
+		// Update input
+		for event in self.event_pump.poll_iter() {
+			match event {
+				Event::Quit { .. } => self.running = false,
+				_ => {},
+			}
+		}
+
+		// Update layers
+		let layer_max = self.layer_stack.len() - 1;
+		for (index, layer) in self.layer_stack.iter_mut().enumerate() {
+			if index == layer_max {
+				layer.update_focused();
+			} else {
+				layer.update_unfocused();
+			}
+			layer.update();
+		}
 	}
 }
