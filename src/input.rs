c663354051dd0c4340526de9103ad2fc25c57029
SalsaGal <fielddashos@gmail.com>
2021-01-18 23:27:48 +1100

    Mouse handling

--- src/input.rs
+++ src/input.rs
@@ -1,14 +1,22 @@
+use crate::math::Vector2;
+
 use sdl2::event::Event;
 
 use std::collections::HashMap;
 
 pub use sdl2::{
 	keyboard::Scancode,
+	mouse::MouseButton,
 };
 
 pub struct InputHandler {
 	keys_down: HashMap<Scancode, bool>,
 	keys_pressed: HashMap<Scancode, bool>,
+
+	buttons_down: HashMap<MouseButton, bool>,
+	buttons_pressed: HashMap<MouseButton, bool>,
+
+	pub mouse_pos: Vector2<i32>,
 }
 
 impl InputHandler {
@@ -16,6 +24,11 @@ impl InputHandler {
 		Self {
 			keys_down: HashMap::new(),
 			keys_pressed: HashMap::new(),
+
+			buttons_down: HashMap::new(),
+			buttons_pressed: HashMap::new(),
+
+			mouse_pos: Vector2::origin(),
 		}
 	}
 	
@@ -28,12 +41,23 @@ impl InputHandler {
 			Event::KeyUp { scancode, .. } => {
 				self.keys_down.insert(scancode.unwrap(), false);
 			},
-			_ => {}//unreachable!()			// Other functions will never be passed
+			Event::MouseButtonDown { mouse_btn, .. } => {
+				self.buttons_down.insert(mouse_btn, true);
+				self.buttons_pressed.insert(mouse_btn, true);
+			}
+			Event::MouseButtonUp { mouse_btn, .. } => {
+				self.buttons_down.insert(mouse_btn, false);
+			}
+			Event::MouseMotion { x, y, .. } => {
+				self.mouse_pos = Vector2::new(x, y);
+			}
+			_ => unreachable!()			// Other functions will never be passed
 		}
 	}
 
 	pub fn update(&mut self) {
 		self.keys_pressed.clear();
+		self.buttons_pressed.clear();
 	}
 
 	pub fn key_is(&self, key: Scancode, state: InputState) -> bool {
@@ -41,6 +65,14 @@ impl InputHandler {
 			InputState::Down => *self.keys_down.get(&key).unwrap_or(&false),
 			InputState::Pressed => *self.keys_pressed.get(&key).unwrap_or(&false),
 			InputState::Up => !self.keys_down.get(&key).unwrap_or(&false),
+		}
+	}
+
+	pub fn button_is(&self, button: MouseButton, state: InputState) -> bool {
+		match state {
+			InputState::Down => *self.buttons_down.get(&button).unwrap_or(&false),
+			InputState::Pressed => *self.buttons_pressed.get(&button).unwrap_or(&false),
+			InputState::Up => !self.buttons_down.get(&button).unwrap_or(&false),
 		}
 	}
 }
