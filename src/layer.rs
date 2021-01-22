fa59af89f56d174c91a95fe6e68ec38ea08058cc
SalsaGal <fielddashos@gmail.com>
2021-01-22 22:19:30 +1100

    Add EmptyLayer

--- src/layer.rs
+++ src/layer.rs
@@ -15,6 +15,20 @@ pub trait GameLayer {
 	fn update(&mut self, _data: &mut GlobalData, _graphics: &mut GraphicsHandler, _input: &InputHandler, _focused: bool) -> Transition { Transition::None }
 }
 
+pub struct EmptyLayer;
+impl GameLayer for EmptyLayer {
+	// When the layer is added to the stack
+	fn on_push(&mut self, _data: &mut GlobalData) {}
+	// When the layer is taken off the stack
+	fn on_pop(&mut self, _data: &mut GlobalData) {}
+	// When a layer is put on top of it
+	fn on_lose_focus(&mut self, _data: &mut GlobalData) {}
+	// WHen a layer is taken off from on top of it
+	fn on_gain_focus(&mut self, _data: &mut GlobalData) {}
+	
+	fn update(&mut self, _data: &mut GlobalData, _graphics: &mut GraphicsHandler, _input: &InputHandler, _focused: bool) -> Transition { Transition::None }
+}
+
 pub enum Transition {
 	None,
 	Pop,
