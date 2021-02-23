274bb67244c30f99bc27784ab9a9d7eb4f5452c1
SalsaGal <fielddashos@gmail.com>
2021-02-24 08:12:31 +1100

    Derive Debug for Vector2

--- src/math.rs
+++ src/math.rs
@@ -6,7 +6,7 @@ use std::ops::{Add, Div, Mul, Sub};
 
 /// A struct which contains two values of the same type, typically used to represent
 /// 2D values, like map positions.
-#[derive(Clone, Copy, Eq, Hash, PartialEq)]
+#[derive(Clone, Copy, Debug, Eq, Hash, PartialEq)]
 pub struct Vector2<T> {
 	pub x: T,
 	pub y: T,
